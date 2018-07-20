50ba898d744caf8b0f100c718be5c54ee9b05f6b
gerkenv <gerkenv@gmail.com>
2018-07-20 22:18:05 +0200

    + link:

--- js/node.js/console-app/console-app/basic/processmodule.js
+++ js/node.js/console-app/console-app/basic/processmodule.js
@@ -1,7 +1,10 @@
 ﻿
+// http://stackabuse.com/command-line-arguments-in-node-js/
+
 // get command-line arguments
 // 0 - name of `node` command
 // 1 - name of a main script
+// [2, ...] - arguments
 module.exports.getArgs = getArgs;
 function getArgs() {
     return process.argv;
