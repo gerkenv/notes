c685aa3ed7f6ab1a5d1447d72679459d99124dbd
Valeriy Gerken <gerkenv@gmail.com>
2018-09-20 22:01:58 +0200

    Authentification service was integrated to register page;

--- js/mean/app/angular-src/src/app/components/register/register.component.ts
+++ js/mean/app/angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
 import { FlashMessagesService } from 'angular2-flash-messages'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -13,8 +15,12 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private _validateService: ValidateService,
-              private _flashMessagesService: FlashMessagesService) { }
+  constructor(
+    private _validateService: ValidateService,
+    private _flashMessagesService: FlashMessagesService,
+    private _authService: AuthService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -43,6 +49,24 @@ export class RegisterComponent implements OnInit {
     return false;
   }
 
+  // register user (send data to backend)
+  this._authService.registerUser(user).subscribe(data => {
+    if (data.success == false) {
+      this._flashMessagesService.show(
+        "Your registration was failed",
+        {cssClass: 'alert-danger', timeout: 3000}
+      );
+      this._router.navigate(['/register']);
+      return false;
+    } else {
+      this._flashMessagesService.show(
+        "You are now registered and can log in",
+        {cssClass: 'alert-success', timeout: 3000}
+      );
+      this._router.navigate(['/login']);
+    }
+  });
+  
     return true;
   }
 
