492ba5167ac120ee5046ea486de78e8e14305325
gerkenv <gerkenv@gmail.com>
2018-09-20 22:58:52 +0200

    Identation, simicolons, etc.;

--- js/mean/app/angular-src/src/app/components/register/register.component.ts
+++ js/mean/app/angular-src/src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
 import { AuthService } from '../../services/auth.service';
-import { FlashMessagesService } from 'angular2-flash-messages'
+import { FlashMessagesService } from 'angular2-flash-messages';
 import { Router } from '@angular/router';
 
 @Component({
@@ -33,40 +33,44 @@ export class RegisterComponent implements OnInit {
     username: this.username,
     email: this.email,
     password: this.password
-  }
+  };
 
   // validate all required fields
   if (!this._validateService.validateRegister(user)) {
-    this._flashMessagesService.show("Please fill up all fields",
-                                    {cssClass: 'alert-danger', timeout: 3000});
+    this._flashMessagesService.show(
+      'Please fill up all fields',
+      {cssClass: 'alert-danger', timeout: 3000}
+    );
     return false;
   }
 
   // validate email
   if (!this._validateService.validateEmail(user.email)) {
-    this._flashMessagesService.show("Please provide a correct email",
-                                    {cssClass: 'alert-danger', timeout: 3000});
+    this._flashMessagesService.show(
+      'Please provide a correct email',
+      {cssClass: 'alert-danger', timeout: 3000}
+    );
     return false;
   }
 
   // register user (send data to backend)
   this._authService.registerUser(user).subscribe(data => {
-    if (data.success == false) {
+    if (data.success === false) {
       this._flashMessagesService.show(
-        "Your registration was failed",
+        'Your registration was failed',
         {cssClass: 'alert-danger', timeout: 3000}
       );
       this._router.navigate(['/register']);
       return false;
     } else {
       this._flashMessagesService.show(
-        "You are now registered and can log in",
+        'You are now registered and can log in',
         {cssClass: 'alert-success', timeout: 3000}
       );
       this._router.navigate(['/login']);
     }
   });
-  
+
     return true;
   }
 
