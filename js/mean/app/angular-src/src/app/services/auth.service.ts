2b67d0820ecce29f4ce9d9d5ba80e0cc64c0de4c
gerkenv <gerkenv@gmail.com>
2018-09-20 22:50:18 +0200

    User authentification is prepared on frontend;

--- js/mean/app/angular-src/src/app/services/auth.service.ts
+++ js/mean/app/angular-src/src/app/services/auth.service.ts
@@ -20,4 +20,13 @@ export class AuthService {
     ).map(res => res.json());
   }
 
+  authenticateUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this._http.post(
+      'http://localhost:3000/users/authenticate',
+      user,
+      {headers:headers}
+    ).map(res => res.json());
+  }
 }
