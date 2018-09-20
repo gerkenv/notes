63cd5752469d257aa01124517f4d4b2acc589067
gerkenv <gerkenv@gmail.com>
2018-09-20 23:58:07 +0200

    Strict comparison instead of soft one;

--- js/mean/app/angular-src/src/app/services/validate.service.ts
+++ js/mean/app/angular-src/src/app/services/validate.service.ts
@@ -6,15 +6,15 @@ export class ValidateService {
   constructor() { }
 
   validateRegister(user) {
-    if ((user.name == undefined) 
-    || (user.username == undefined) 
-    || (user.email == undefined) 
-    || (user.password == undefined)) {
+    if ((user.name === undefined)
+    || (user.username === undefined)
+    || (user.email === undefined)
+    || (user.password === undefined)) {
       return false;
     }
     return true;
   }
-  
+
   validateEmail(email) {
     const re = /^[\w-.]+@[\w-]+.[\w-.]+/;
     return re.test(email);
