bd554d53708896814a1b996e9728ea9a157fea6d
Valeriy Gerken <gerkenv@gmail.com>
2018-09-21 22:24:22 +0200

    Profile component is prepared;

--- js/mean/app/angular-src/src/app/components/profile/profile.component.ts
+++ js/mean/app/angular-src/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+// user: String;
+// username: String;
+// email: String;
+// password: String;
+user: object;
 
-  constructor() { }
+  constructor(
+    private _authService: AuthService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this._authService.getProfile().subscribe(profile => {
+      this.user = profile.user;
+      // this.username = profile.username;
+      // this.email = profile.email;
+      // this.password = profile.password;
+    },
+    err => {
+      console.log(err);
+      return false;
+    }
+  );
   }
 
 }
