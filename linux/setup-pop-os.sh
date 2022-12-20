cd14347a3ddd38bbb9ed026fbb7455077b217287
Valeriy Gerken <gerkenv@gmail.com>
2022-12-20 04:18:02 +0100

    Update setup-pop-os.sh

--- linux/setup-pop-os.sh
+++ linux/setup-pop-os.sh
@@ -16,3 +16,13 @@ which python3
 # https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH#install-and-set-up-zsh-as-default
 sudo apt install zsh
 
+# install `oh-my-zsh`
+
+# install fzf
+
+# check if `git` is installed
+which git
+
+# git. configure default branch name to be `main`
+
+# install kinto (replicate macos keybindings)
