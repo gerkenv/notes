8d42ad5b3bfb6d55bf0a7f17f42e87b072586827
Valeriy Gerken <gerkenv@gmail.com>
2022-12-20 21:39:47 +0100

    Update setup-pop-os.sh

--- linux/setup-pop-os.sh
+++ linux/setup-pop-os.sh
@@ -12,17 +12,24 @@ sudo apt-get install google-chrome-stable
 which python
 which python3
 
+# install performance monitor `htop`
+sudo apt install htop
+
 # install `zsh`
 # https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH#install-and-set-up-zsh-as-default
 sudo apt install zsh
 
 # install `oh-my-zsh`
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install fzf
+
 
 # check if `git` is installed
 which git
 
 # git. configure default branch name to be `main`
+git config --global init.defaultBranch main
 
 # install kinto (replicate macos keybindings)
+/bin/bash -c "$(wget -qO- https://raw.githubusercontent.com/rbreaves/kinto/HEAD/install/linux.sh || curl -fsSL   https://raw.githubusercontent.com/rbreaves/kinto/HEAD/install/linux.sh)"
