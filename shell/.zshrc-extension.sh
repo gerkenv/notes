bfd8792f08c75f775cd2ec267a465296d09c7ddd
Valeriy Gerken <gerkenv@gmail.com>
2022-12-20 02:28:59 +0100

    Create .zshrc-extension.sh

---
+++ shell/.zshrc-extension.sh
@@ -0,0 +1,9 @@
+# # Add timestamp to the command line prompt
+# PROMPT='%{$fg[yellow]%}[%D{%f/%m/%y} %D{%L:%M:%S}] '$PROMPT
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
